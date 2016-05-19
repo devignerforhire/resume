c49e1af70fc7cfe950b9e141db52ee34583f2125
Chris Rault <chris@devignerforhire.com>
2016-05-19 13:29:50 +0200

    Revised the js and implemented the toggle.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,59 @@
-$(document).ready(function($) {
-	$('.accordion').find('.accordion-toggle').click(function(){
+!(function( $ ) {
 
-		// Expand or collapse the panel
-		$(this).next().slideToggle('fast');
+    var dfh = function() {
 
-		// Hide the other panels
-		$(".accordion-content").not($(this).next()).slideUp('fast');
+        this.listen();
 
-	});
-});
+    }
+
+    dfh.prototype = {
+
+    	constructor: dfh,
+
+        toggleProjects: function() {
+
+            $( '.projects' ).toggle();
+
+            if ( $( '.projects' ).is( ':visible' ) ) {
+				$( 'button.show-more span' ).html('Show less');
+            } else {
+				$( 'button.show-more span' ).html('Show more');
+            }
+
+        },
+        listen: function() {
+
+            var $this = this;
+
+
+            $( document ).on( 'click', 'button.show-more', function( e ) {
+
+                e.preventDefault();
+
+                $this.toggleProjects();
+
+            } );
+
+        }
+
+    };
+
+    // Fire js
+    $( document ).ready( function() {
+
+        new dfh();
+
+		$('.accordion').find('.accordion-toggle').click(function(){
+
+			// Expand or collapse the panel
+			$(this).next().slideToggle('fast');
+
+			// Hide the other panels
+			$(".accordion-content").not($(this).next()).slideUp('fast');
+			$(".accordion-content i").not($(this).next()).addClass('down-icon')
+
+		});
+
+    });
+
+})( window.jQuery );
